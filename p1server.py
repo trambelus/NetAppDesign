eef173e81678ad7553dff0b790c8e75e8d7e8095
trambelus <trambelus2@gmail.com>
2015-10-03 18:32:43 -0400

    Converted p1server.py to tabs

--- p1server.py
+++ p1server.py
@@ -30,7 +30,7 @@ def flash(is_active):
 	while True:
 		c = 1-c
 		if len(is_active) == 0: # empty list means exit, for our purposes
-            GPIO.output(LED,GPIO.LOW) # Turn off LED
+			GPIO.output(LED,GPIO.LOW) # Turn off LED
 			break # jump out of this infinite while loop and exit this thread
 		if is_active[0]:
 			if c:
@@ -42,56 +42,56 @@ def flash(is_active):
 		time.sleep(FLASH_DELAY)
 
 try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind((TCP_IP, TCP_PORT))
-        s.listen(1)
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s.bind((TCP_IP, TCP_PORT))
+		s.listen(1)
 
-        while 1:
-            is_active = [False] # It's a list because it'll get passed to the thread by reference this way, not by value.
-            # If we just passed False as an argument, changing the local variable here wouldn't change the thread's variable.
-            # There are many ways to implement this, but I like this list-singleton method. It's simple.
-            Thread(target=flash, args=(is_active,)).start() # start the thread
-	    # Setup GPIO as output
-	    GPIO.setmode(GPIO.BOARD)
-	    GPIO.setup(LED, GPIO.OUT)
-            conn, addr = s.accept()
-            print 'Connection address:', addr
-            recv_data = conn.recv(BUFFER_SIZE)
-            print "received data: ", recv_data
-            if recv_data == '0': # If receive 0 turn LED OFF
-                print "INSIDE '0' of IF"
-                is_active[0] = Falsh # Turns the flashing off
-                GPIO.output(LED,GPIO.LOW)
-                send_data = '0'
-                conn.send(str(send_data))  # Send ACK to RPI1
-                GPIO.cleanup()
-                conn.close() # Close socket connection
-            elif recv_data == '1': # If receive 1 turn LED ON
-                print "INSIDE '1' of ELIF"
-                is_active[0] = Falsh # Turns the flashing off
-                GPIO.output(LED,GPIO.HIGH)
-                send_data = '0'
-                conn.send(str(send_data))  # Send ACK to RPI1
-                GPIO.cleanup()
-                conn.close() # Close socket connection
-            elif recv_data == '2':           # This is just for now, will need to incorporate thread here; If receive 2 blink LED
-                # blinkLED()
-                print "INSIDE '2' of ELIF"
-                is_active[0] = True # Turns the flashing on
-                send_data = '0'
-                conn.send(str(send_data))  # Send ACK to RPI1
-                GPIO.cleanup()
-                conn.close() # Close socket connection
-            # else :
-                # print "INSIDE ELSE"
-                # GPIO.output(LED,GPIO.HIGH)
-                # time.sleep(0.5)
-                # GPIO.output(LED,GPIO.LOW)
-                # time.sleep(0.5)
-                # GPIO.output(LED,GPIO.HIGH)
-                # send_data = '1'
-                # conn.send(str(send_data))  # Send ACK to RPI1
+		while 1:
+			is_active = [False] # It's a list because it'll get passed to the thread by reference this way, not by value.
+			# If we just passed False as an argument, changing the local variable here wouldn't change the thread's variable.
+			# There are many ways to implement this, but I like this list-singleton method. It's simple.
+			Thread(target=flash, args=(is_active,)).start() # start the thread
+			# Setup GPIO as output
+			GPIO.setmode(GPIO.BOARD)
+			GPIO.setup(LED, GPIO.OUT)
+			conn, addr = s.accept()
+			print 'Connection address:', addr
+			recv_data = conn.recv(BUFFER_SIZE)
+			print "received data: ", recv_data
+			if recv_data == '0': # If receive 0 turn LED OFF
+				print "INSIDE '0' of IF"
+				is_active[0] = Falsh # Turns the flashing off
+				GPIO.output(LED,GPIO.LOW)
+				send_data = '0'
+				conn.send(str(send_data))  # Send ACK to RPI1
+				GPIO.cleanup()
+				conn.close() # Close socket connection
+			elif recv_data == '1': # If receive 1 turn LED ON
+				print "INSIDE '1' of ELIF"
+				is_active[0] = Falsh # Turns the flashing off
+				GPIO.output(LED,GPIO.HIGH)
+				send_data = '0'
+				conn.send(str(send_data))  # Send ACK to RPI1
+				GPIO.cleanup()
+				conn.close() # Close socket connection
+			elif recv_data == '2':           # This is just for now, will need to incorporate thread here; If receive 2 blink LED
+				# blinkLED()
+				print "INSIDE '2' of ELIF"
+				is_active[0] = True # Turns the flashing on
+				send_data = '0'
+				conn.send(str(send_data))  # Send ACK to RPI1
+				GPIO.cleanup()
+				conn.close() # Close socket connection
+			# else :
+				# print "INSIDE ELSE"
+				# GPIO.output(LED,GPIO.HIGH)
+				# time.sleep(0.5)
+				# GPIO.output(LED,GPIO.LOW)
+				# time.sleep(0.5)
+				# GPIO.output(LED,GPIO.HIGH)
+				# send_data = '1'
+				# conn.send(str(send_data))  # Send ACK to RPI1
 except KeyboardInterrupt:
-        is_active.remove(0) # Turns the flashing off
-        GPIO.cleanup()
-        conn.close() # Close socket connection
+		is_active.remove(0) # Turns the flashing off
+		GPIO.cleanup()
+		conn.close() # Close socket connection
