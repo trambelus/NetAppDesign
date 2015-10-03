e56168131aa2cc809387a1091931b95c24255467
ccox04 <ccox04@vt.edu>
2015-10-03 13:33:05 -0400

    server1.2
    Fixed server crashing.  Server continually runs when a client connects
    or disconnects. Now I need to implement the thread for the flashing and
    the server is complete.

--- ChrisCoxServerP1.py
+++ ChrisCoxServerP1.py
@@ -12,7 +12,7 @@ import RPi.GPIO as GPIO
 # GPIO pin number of LED according to spec; GPIO pin 18 Phys Pin 12
 LED = 12
 
-TCP_IP = '192.168.0.12'
+TCP_IP = '172.30.147.125'
 # TCP_IP = '0.0.0.0'
 TCP_PORT = 45678
 BUFFER_SIZE = 2  # Normally 1024, but we want fast response
@@ -30,14 +30,16 @@ GPIO.output(LED, GPIO.LOW)
     #etc.
 
 try:
-    while 1:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.bind((TCP_IP, TCP_PORT))
         s.listen(1)
 
-        conn, addr = s.accept()
-        print 'Connection address:', addr
         while 1:
+	    # Setup GPIO as output
+	    GPIO.setmode(GPIO.BOARD)
+	    GPIO.setup(LED, GPIO.OUT)
+            conn, addr = s.accept()
+            print 'Connection address:', addr
             recv_data = conn.recv(BUFFER_SIZE)
             print "received data: ", recv_data
             if recv_data == '0': # If receive 0 turn LED OFF
