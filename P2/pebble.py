116ddb065800d4df26a3b4a5f59aa2130ac355fd
trambelus <trambelus2@gmail.com>
2015-10-30 18:52:50 -0400

    Added more arguments; started pika stuff
    Yay for options.

--- P2/pebble.py
+++ P2/pebble.py
@@ -12,6 +12,9 @@ import time
 import zeroconf
 
 LOGFILE = 'pebble.log'
+SERVER_Q = 'bottle_q'
+AUTHOR_DEFAULT = 'John'
+AGE_DEFAULT = 20
 
 def log(*msg, additional='', console_only=False):
 	"""
@@ -23,6 +26,15 @@ def log(*msg, additional='', console_only=False):
 		with open(LOGFILE, 'a') as f:
 			f.write(output + '\n')
 
+def create_request(subject, message):
+	pass
+
+def send(json_msg):
+	conn = pika.BlockingConnection(pika.ConnectionParameters(host=HOST))
+	channel = conn.channel()
+	channel.queue_declare(queue=SERVER_Q)
+	channel.basic_publish(exchange='', routing_key=SERVER_Q, body=json_msg)
+
 def push(msg):
 	pass
 
@@ -33,22 +45,29 @@ def pullr():
 	pass
 
 def process_args(argv):
+	# Setup and parse
 	parser = argparse.ArgumentParser(description='Sends and receives messages to/from a bottle')
-	parser.add_argument('-a', dest='action', choices=['push','pull','pullr'], required=True)
-	parser.add_argument('-s', dest='subject', nargs='+')
-	parser.add_argument('-m', dest='message', nargs='+')
-	parser.add_argument('-Qm', dest='messageQ', nargs='+')
-	parser.add_argument('-Qs', dest='subjectQ', nargs='+')
+	parser.add_argument('-a', dest='action', choices=['push','pull','pullr'], required=True, help='Action to do')
+	parser.add_argument('-s', dest='subject', nargs='+', help='Subject to send')
+	parser.add_argument('-m', dest='message', nargs='+', help='Message to send')
+	parser.add_argument('-Qm', dest='messageQ', nargs='+', help='Message to request (regex)')
+	parser.add_argument('-Qs', dest='subjectQ', nargs='+', help='Subject to request (regex)')
 	parser.add_argument('-Qa', dest='ageQ')
+	parser.add_argument('--age', type=int, required=False, default=AGE_DEFAULT, help='Age of author to send')
+	parser.add_argument('--author', type=string, required=False, default=AUTHOR_DEFAULT, help='Author name to send')
+	parser.add_argument('--host', default=HOST, help='Hostname of the RabbitMQ server to connect to')
 	args = parser.parse_args(argv)
+	# Validate and join
 	if args.action == 'push':
 		if not (args.subject and args.message):
 			parser.error("the following arguments are required for push: -s, -m")
 		args.subject = ' '.join(args.subject)
 		args.message = ' '.join(args.message)
 	if args.action == 'pull' or args.action == 'pullr':
-		if not (args.messageQ or args.subjectQ or args.ageQ):
-			parser.error("one of the following arguments is required for %s: -Qm, -Qs, Qa" % args.action)
+		if args.messageQ:
+			args.messageQ = ' '.join(args.messageQ)
+		if args.subjectQ:
+			args.subjectQ = ' '.join(args.subjectQ)
 	return args
 
 		
