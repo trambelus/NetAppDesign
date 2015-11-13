df210a14981706a1f3c3b2da2e4045a1b9662c12
ccox04 <ccox04@vt.edu>
2015-11-13 18:45:26 -0500

    Fixed test text. it works

--- P3/testtext.py
+++ P3/testtext.py
@@ -4,7 +4,7 @@ from twilio.rest import TwilioRestClient
 client = TwilioRestClient(account='ACe3446369fe6f831be04eae238e9bdfa8', token='67fef0ff1be5813a0a162b22200ae2b7')
 
 def sendMessage():
-	client.messages.create(to='+15406135061', from_='+15406135061', body="test test message")
+	client.messages.create(to='+15407974693', from_='+15406135061', body="test test message")
 	print('Just ran sendMessage', client)
 
 
