66c510e6fe1ab4106678da2be18a146eda49a1a5
trambelus <trambelus2@gmail.com>
2015-11-13 15:25:14 -0500

    Added weather

--- P3/icu.py
+++ P3/icu.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
-import argparse
+import argparse	# argument parsing
 import ephem 		# for the orbit calculations
 import requests	# to get Celestrak TLEs and NOAA data
+import time			# gets the current date
 
 def log(*msg):
 	"""
@@ -14,6 +15,9 @@ def log(*msg):
 		f.write(output + '\n')
 
 def parse_args():
+	"""
+	Parses input args and returns a namespace with 'zip' and 'sat' containing zip and sat codes.
+	"""
 	parser = argparse.ArgumentParser(
 		prog='ICU',
 		description="Given a zip code and satellite code, generates notifications " +
@@ -22,3 +26,9 @@ def parse_args():
 	parser.add_argument('-z', dest='zip', type=int, required=True, help="Zip code for which to check viewable events")
 	parser.add_argument('-s', dest='sat', required=True, help="Name of satellite to view")
 	return parser.parse_args()
+
+def get_weather(zipcode):
+	# http://www.ncdc.noaa.gov/cdo-web/api/v2/data?datasetid=GHCND&locationid=ZIP:28801&startdate=2010-05-01&enddate=2010-05-01
+	today = time.strftime('%Y-%m-%d')
+	params = {'datasetid':'GHCND', 'locationid':'ZIP:%05d'%zipcode, 'startdate':today, 'enddate':today}
+	response = requests.get('http://www.ncdc.noaa.gov/cdo-web/api/v2/data', params=params)
