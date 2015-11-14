31ab6c7cae9ff13d5668f969654a15c14aead27c
Trambelus <trambelus2@gmail.com>
2015-11-14 14:14:23 -0500

    PiSSH: Updated client and startup script

--- pissh/pissh-client.py
+++ pissh/pissh-client.py
@@ -3,21 +3,35 @@
 import requests
 import time
 import argparse
+import os
+
+def log(*msg):
+	"""
+	Prepends a timestamp and prints a message to the console and LOGFILE
+	"""
+	output = "%s:\t%s" % (time.strftime("%Y-%m-%d %X"), ' '.join([str(s) for s in msg]))
+	print(output)
+	try:
+		with open(LOGFILE, 'a') as f:
+			f.write(output + '\n')
+	except:
+		return
 
 def main():
 	parser = argparse.ArgumentParser()
-	parser.add_argument('id', required=True, help='ID of Pi to search for')
+	parser.add_argument('-id', default='T25Pi', help='ID of Pi to search for')
 	args = parser.parse_args()
 	pi_id = args.id
 	while True:
 		try:
-			resp = response.get('http://jenna.xen.prgmr.com/pull?id=%s' % pi_id)
+			resp = requests.get('http://jenna.xen.prgmr.com:5000/pissh/pull?id=%s' % pi_id)
 			if resp.text != '':
-				log("Found IP: %s" % resp.text)
+				log("Found IP: attempting connection to %s" % resp.text)
 				os.system('putty -ssh %s' % resp.text)
-			time.sleep(1)
+				return
 		except Exception as ex:
 			log(ex)
+		time.sleep(1)
 
 if __name__ == '__main__':
 	main()
