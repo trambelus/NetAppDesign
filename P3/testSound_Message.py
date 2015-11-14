3f4f23f2cd769f3f52cefc9d52fecd8333cba20a
ccox04 <ccox04@vt.edu>
2015-11-13 19:30:00 -0500

    took out wav file

--- P3/testSound_Message.py
+++ P3/testSound_Message.py
@@ -7,8 +7,6 @@ client = TwilioRestClient(account='ACe3446369fe6f831be04eae238e9bdfa8', token='6
 def sendMessage():
 	client.messages.create(to='+15407974693', from_='+15406135061', body="Playing Hello Mister Gopher", media_url="http://www.bassmaster.com/sites/default/files/imagecache/slideshow_image/06DeanRojas_Record.jpg")
 	print('Just ran sendMessage', client)
-	client.messages.create(to='+15407974693', from_='+15406135061', body="Playing Hello Mister Gopher", media_url="http://www.wavsource.com/snds_2015-11-01_1874590815319647/tv/a-team/a-team_intro.wav")
-	print('Just ran sendMessage for wav file', client)
 	pygame.mixer.init()
 	pygame.mixer.music.load("hello_mister_gopher.wav")
 	pygame.mixer.music.play()
