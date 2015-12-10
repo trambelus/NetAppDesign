9e9c7791a6c82943bdb29751e8360356b9abc200
trambelus <trambelus2@gmail.com>
2015-12-10 16:16:03 -0500

    Added traces, render template

--- final/server.py
+++ final/server.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request, flash, redirect, url_for
+from flask import Flask, request, flash, redirect, url_for, render_template
 from pprint import pprint
 import hashlib
 
@@ -8,30 +8,37 @@ app = Flask(__name__)
 
 @app.route('/rooms')
 def rooms():
+	pprint(request)
 	with open('index.html') as f:
 		index_html = '\n'.join(f.readlines())
 	return index_html
 
 @app.route('/rooms/upload', methods=['GET','POST'])
 def upload():
+	pprint(request)
 	if request.method == 'POST':
 		f = request.files['latest.png']
 		f.save('/img/latest.png')
 
 @app.route('/rooms/validate', methods=['GET','POST'])
 def validate():
+	pprint(request)
 	if request.method == 'POST':
 		with open('hash.txt','r') as f:
 			hash_s = f.readlines()[0]
+
 		hash_f = hashlib.new('sha256')
 		hash_f.update(bytes(request.form1['password'],'UTF-8'))
 		if hash_f.hex_digest() != hash_s:
-			abort(401)
+			return render_template('index.html',error='Invalid password')
+
 		flash('Authentication successful')
 		return redirect(url_for('/rooms/display'))
+	return render_template('index.html',error='Invalid password')
 
 @app.route('/rooms/display', methods=['GET'])
 def display():
+	pprint(request)
 	with open('results.html') as f:
 		results_html = '\n'.join(f.readlines())
 	return results_html
